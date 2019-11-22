c3af289d31081c69471e6107532944acd93184b3
Phil Breen <pbreen@ITs-MacBook-Pro.local>
2019-11-22 13:33:37 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+#include <fstream>
+
+#include <vector>
+#include <cmath>
+#include <numeric> 
+#include <cstdlib>
+
+#include "mpreal.h"
+using namespace mpfr;
+
+#include "Brutus.h"
+
+int main(int argc, char* argv[]) {
+    // using mpfr::mpreal;    
+  int n;
+  mpreal t_end;
+  mpreal eta = "0.24";
+  mpreal t = "0";
+  mpreal epsilon; // = "1e-6"; // Bulirsch-Stoer tolerance
+  int numBits;	
+  double dt;
+
+  cin >> n >> t_end >> dt >> epsilon >> numBits;
+  cout << "# n=" << n << " tend=" << t_end <<  " dt=" << dt << " eps=" << epsilon  <<  " lw=" << numBits <<endl;
+  //store data in vector
+  vector<double> data(7*n);
+  vector<mpreal> datmp(7*n);
+  double temp;
+  for (int i = 0; i < data.size(); ++i){
+      cin >> data[i];
+      datmp[i] = (mpreal)data[i];
+      //cout << datmp[i] << " " <<  i << endl;
+   }
+ 
+   //Brutus brutus = Brutus(datmp);
+   Brutus brutus = Brutus(t, datmp, epsilon, numBits);
+
+   vector<string> v = brutus.get_data_string();
+   for (int i = 0; i < v.size(); ++i){
+	cout << v[i] << " ";
+   }
+   cout << endl;
+
+   vector<mpreal> e3;
+   mpreal e30 = brutus.get_energies()[0];
+   while(t<t_end) {
+	t+=dt;
+   	brutus.evolve((mpreal)t);
+   	v = brutus.get_data_string();
+	e3 = brutus.get_energies();
+	
+	cout << t << " " << (e3[0]/e30-1.0); 
+   	for (int i = 0; i < v.size(); ++i){
+		cout << v[i] << " ";
+	   }
+	   cout << endl;
+   }
+  //Brutus intB = Brutus();
+		
+  return 0;
+}
+
+
+
+    
+
